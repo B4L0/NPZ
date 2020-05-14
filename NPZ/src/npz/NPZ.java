4f038bea2f310a4e4af3eba0916bbeb4e5c79f16
Kuba-PISI <Kuba-PISI@DESKTOP-UU7TF7L>
2020-05-14 19:22:29 +0200

    test wersji ostatecznej

--- NPZ/src/npz/NPZ.java
+++ NPZ/src/npz/NPZ.java
@@ -1,9 +1,73 @@
 package npz;
 
-public class NPZ {
-    
-    public static void main(String[] args) {
-        
+public class NPZ
+{
+
+    public static void main(String[] args) 
+    {
+        Pomieszczenie[] mojePomieszczenia = {
+            new Pomieszczenie() {
+                {
+                    this.szerokosc = 3;
+                    this.dlugosc = 2;
+                    this.nazwa = "pokoj";
+                }
+            },
+            new Pomieszczenie() {
+                {
+                    szerokosc = 2;
+                    dlugosc = 2;
+                    nazwa = "lazienka";
+                }
+            } 
+        };
+
+        Mieszkanie mojeMieszkanie = new Mieszkanie() {
+            {
+                this.pomieszczenia = mojePomieszczenia;
+                this.wizytowka = "Kowalscy";
+            }
+        };
+
+        ElementPietra[] mojeElementyParteru = {
+            mojeMieszkanie,
+            new Pomieszczenie() {
+                {
+                    szerokosc = 2;
+                    dlugosc = 2;
+                    nazwa = "suszarnia";
+                }
+            }
+        };
+
+        Pietro[] mojePietra = {
+            new Pietro() {
+                {
+                    numerPietra = 0;
+                    elementy = mojeElementyParteru;
+                }
+            }
+        };
+
+        Garaz[] mojeGaraze = {
+            new Garaz() {
+                {
+                    szerokoscBramy = 3;
+                    wysokoscBramy = 2;
+                    szerokosc = 5;
+                    dlugosc = 5;
+                }
+            }
+        };
+
+        Budynek mojBudynek = new Budynek() {
+            {
+                powierzchniaDzialki = 20;
+                pietra = mojePietra;
+                garaze = mojeGaraze;
+            }
+        };
+
+        System.out.println(mojBudynek.PoliczPole());
     }
-    
 }
